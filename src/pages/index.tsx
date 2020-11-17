2741f779b1ae8b090cc02b64ba0bca87bdc1981b
freeshineit <xiaoshaoqq@gmail.com>
2020-11-18 00:01:51 +0800

    feat: 首页 背景

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,46 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
 import styles from "./Index.module.scss";
 import ShineShao from "@components/ShineShao";
 
-const Index = () => (
-  <div className={styles.container}>
-    <ShineShao />
-  </div>
-);
+const bgList = [1, 2, 3];
+
+const Index = () => {
+  const [bg, setBg] = useState(bgList[0]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const random = ~~(Math.random() * 3);
+      setBg(bgList[random]);
+    }, 5000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div
+      className={styles.container}
+      style={{ backgroundImage: `url(/static/images/bg/${bg}.jpeg)` }}
+    >
+      <div className={styles.cover}></div>
+      <div className={styles.content}>
+        <div className={styles.me}>
+          <ShineShao className={styles.face} />
+          <h3> I'm ShineShao， FE Developer</h3>
+          <div className={styles.links}>
+            <Link href="/blog">
+              <a>BLOG</a>
+            </Link>
+            <Link href="/blog">
+              <a>GITHUB</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 Index.displayName = "tsx";
 export default Index;
