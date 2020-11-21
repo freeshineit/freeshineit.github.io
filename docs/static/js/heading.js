9496feff53886c3e010f658bea1f8fece9f1f7ae
freeshineit <xiaoshaoqq@gmail.com>
2020-11-22 01:35:17 +0800

    build

---
+++ docs/static/js/heading.js
@@ -0,0 +1,9 @@
+(function () {
+  let headingsObj = {};
+
+  const headings = document.querySelectorAll("h2[id],h3[id]");
+
+  for (let heading of headings) {
+    console.log(heading.tagName);
+  }
+})();
