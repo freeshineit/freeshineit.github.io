b90b8ea2c7e522417546a91f18890bcf5ef532fd
freeshineit <xiaoshaoqq@gmail.com>
2020-11-22 17:03:37 +0800

    prefers-color-scheme

--- public/static/js/heading.js
+++ public/static/js/heading.js
@@ -6,4 +6,33 @@
   for (let heading of headings) {
     // console.log(heading.tagName);
   }
+
+  // function handleChange(mediaQueryListEvent) {
+  //   if (mediaQueryListEvent.matches) {
+  //     // 用户切换到了暗色(dark)主题
+  //   } else {
+  //     // 用户切换到了亮色(light)主题
+  //   }
+  // }
+
+  // const mediaQueryListDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+  // // 添加主题变动监控事件
+  // mediaQueryListDark.addEventListener(handleChange);
+
+  // // 移除主题变动监控事件
+  // mediaQueryListDark.removeEventListener(handleChange);
+
+  const mediaQueryListDark = window.matchMedia("(prefers-color-scheme: dark)");
+  if (mediaQueryListDark.matches) {
+    // 系统当前是暗色(dark)主题
+    console.log("系统当前是暗色(dark)主题");
+  }
+
+  const mediaQueryListLight = window.matchMedia(
+    "(prefers-color-scheme: light)"
+  );
+  if (mediaQueryListLight.matches) {
+    // 系统当前是亮色(light)主题
+  }
 })();
