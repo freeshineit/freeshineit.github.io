1fe31569c412dbbf6c358e8ab03f7ca6f67d3b9b
ShineShao <xiaoshaoqq@gmail.com>
2022-10-13 21:56:36 +0800

    update

--- src/components/MDXHead/index.tsx
+++ src/components/MDXHead/index.tsx
@@ -10,7 +10,7 @@ const MDXHead: FC<IMDXHeadProps> = ({ meta }) => (
   <>
     <Head>
       <title>{`${meta.title} (ShineShao)`}</title>
-      <meta name="description" content={meta.description || "ShineShao 博客"} />
+      <meta name="description" content={`${meta.description} ShineShao 博客`} />
       <meta name="keywords" content="ShineShao freeshineit Next.js Blog " />
     </Head>
   </>
